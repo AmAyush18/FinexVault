19f7734638ddfd15c7758b81c65843e1834116a6
Ayush Dwivedi <amayush0818@gmail.com>
2024-02-02 00:26:55 +0530

    Routes and other layout elements added

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Home from './Home/Home';
+import Careers from './Careers/Careers'
+import About from "./About/About";
+import Security from "./Security/Security";
+import Signin from "./Auth/Signin";
+import Signup from "./Auth/Signup";
 
 export {
     Header,
     Footer,
-    Home
+    Home,
+    Careers,
+    About,
+    Security,
+    Signin,
+    Signup
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,25 @@ import './index.css'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Layout from './Layout'
-import { Home } from './components'
+import {
+  Home,
+  Careers,
+  About,
+  Security,
+  Signin,
+  Signup 
+} from './components'
 
 const Root = () => (
   <Router>
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route path='' element={<Home />} />
+        <Route path='careers' element={<Careers />} />
+        <Route path='about' element={<About />} />
+        <Route path='security' element={<Security />} />
+        <Route path='signin' element={<Signin />} />
+        <Route path='signup' element={<Signup />} />
       </Route>
     </Routes>
   </Router>
